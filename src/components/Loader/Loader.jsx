2a1e35bb648b900360bf1d0e00799b7dbbd67036
Sebastian1910 <150168659+Sebastian1910@users.noreply.github.com>
2024-06-14 19:21:28 +0300

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Puff } from "react-loader-spinner";
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import "react-loader-spinner";
 import "./Loader.css";
 
 const LoaderComponent = () => {
